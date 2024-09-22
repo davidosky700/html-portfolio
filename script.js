0f838ce9af260538e9747f90b7242be534794c14
davidosky700 <131352408+davidosky700@users.noreply.github.com>
2024-09-22 22:47:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// function  addNewClass() {
+// 	var div = document.querySelector('.my-row');
+// 	if (window.matchMedia('(max-width: 600px)').matches) {
+// 		div.classList.add('flex-column-reverse');
+// 	} else {
+// 		div.classList.remove('flex-column-reverse');
+// 	}
+// }
